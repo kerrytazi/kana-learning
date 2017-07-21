a8487d8eee3102003345ede7ae2f30da4585f0ff
kerrytazi <kerrytazi@mail.ru>
2017-07-22 03:49:02 +0900

    mobile support

--- index.js
+++ index.js
@@ -1,39 +1,47 @@
 "use strict";
 
 
-let http = require("http");
-let url  = require("url");
-let send = require("./lib/send.js");
-let init = require("./lib/init.js");
+let http     = require("http");
+let url      = require("url");
+let send     = require("./lib/send.js");
+let init     = require("./lib/init.js");
 
 
+const REGEX_MOBILE = /mobi/i;
 const PORT = process.env.PORT || 80;
 const FILES = init([__dirname, "src"], [
     {
-        "short": "pcHtml",
-        "type": "html",
-        "path": "index.pc.pug"
+        "short": "pc",
+        "type":  "html",
+        "path":  "index.pc.pug"
+    },
+    {
+        "short": "mobile",
+        "type":  "html",
+        "path":  "index.mobile.pug"
     },
     {
         "short": "favicon",
-        "type": "icon",
-        "path": "images/favicon.png"
+        "type":  "icon",
+        "path":  "images/favicon.png"
     }
 ]);
 
 
 http.createServer((req, res) => {
-    try {
+    // try {
         let { pathname } = url.parse(req.url);
         switch (pathname) {
             case "/":
-                return send(req, res, FILES.pcHtml);
+                return REGEX_MOBILE.test(req.headers["user-agent"]) ?
+                    send(req, res, FILES.mobile) :
+                    send(req, res, FILES.pc);
                 break;
             case "/favicon/":
                 return send(req, res, FILES.favicon);
                 break;
         }
-    } catch(e) {}
+    // } catch(e) {}
     
     res.statusCode = 404;
     res.end();
